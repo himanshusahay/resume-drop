0dd182eca8a54e564fe6bb96e87aa374571987cf
neoterraforma <himanshu23@icloud.com>
2016-03-16 02:51:20 -0400

    fixed uploading

--- mysite/resumedrop/models.py
+++ mysite/resumedrop/models.py
@@ -3,9 +3,14 @@ from __future__ import unicode_literals
 from django.db import models
 from django import forms
 from django.utils.encoding import python_2_unicode_compatible
+# import os
 
 # for student profile creation
 # -------------
+
+def content_file_name(instance, filename):
+	return '/'.join(['documents', instance.wpi_username])
+
 
 @python_2_unicode_compatible  # to support python 2
 class Student(models.Model):
@@ -14,7 +19,7 @@ class Student(models.Model):
 	class_year = models.CharField(max_length = 15, default="") # could be a master's student
 	major = models.CharField(max_length = 80, default="")
 	# where will the resumes be stored?
-	resume = models.FileField(upload_to='documents/%Y/%m/%d', null=True)
+	resume = models.FileField(upload_to=content_file_name)
 	
 	def __str__(self):
 		return self.name
