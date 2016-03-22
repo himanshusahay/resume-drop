afaad4a9abb8b045d1c19a4ea9e9df1ce1c5099b
Himanshu Sahay <himanshusahay@github.com>
2016-03-22 15:39:11 -0700

    set DEBUG=False

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3)a(u-im-v28w&o4m85yr1l*&%e-lkebwkxblk@$cs@#&j$d#9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
