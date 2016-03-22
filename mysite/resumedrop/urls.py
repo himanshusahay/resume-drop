2b2debe0ae07d028ca71490a9194918c1ff6e8d5
Himanshu Sahay <himanshusahay@github.com>
2016-03-21 21:41:37 -0700

    trying to change the index url

--- mysite/resumedrop/urls.py
+++ mysite/resumedrop/urls.py
@@ -10,7 +10,7 @@ from django.conf.urls.static import static
 app_name = 'resumedrop'
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
+	url(r'', views.index, name='index'),
 	url(r'^create_success/$', views.create_success, name='create_success'),
 	url(r'^success/$', views.success_redirect, name='success_redirect'), 
 	# ex: resumedrop/student_create
