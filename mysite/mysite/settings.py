aaed74b17ad299a12ce2356147dee5e22e9d8c23
Himanshu Sahay <himanshusahay@github.com>
2016-03-21 17:36:18 -0700

    added import env

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 # Update database configuration with $DATABASE_URL.
 import dj_database_url
+import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
